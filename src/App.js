457d9d131f3ccc41bc8ce7c5f5dff85e48d26a46
Fat-Tony <fat_tonyy@icloud.com>
2019-12-16 16:01:35 +0000

    UseState-setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
 import Alert from "./components/Alert";
+import uuid from "uuid/v4";
+
+const initialExpenses = [
+  { id: uuid(), charge: "rent", amount: 1600 },
+  { id: uuid(), charge: "food", amount: 500 },
+  { id: uuid(), charge: "drink", amount: 2600 }
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
     <>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>Budget Calculator</h1>
+
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+
+      <h1>
+        total spending :{" "}
+        <span className="total">
+          GHC{" "}
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
     </>
   );
 }
