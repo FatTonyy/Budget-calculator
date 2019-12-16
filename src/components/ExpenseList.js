3b75839090302aa0623069090a649fb7dd127e5b
Fat-Tony <fat_tonyy@icloud.com>
2019-12-16 16:52:09 +0000

    Expenselist Component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Item from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
 
-export default function ExpenseList() {
+export default function ExpenseList({ expenses }) {
   return (
-    <div>
-      <h6>hello from expense list</h6>
-      <Item />
-    </div>
+    <>
+      <ul className="list">
+        {expenses.map(expense => {
+          return <Item key={expense.id} expense={expense} />;
+        })}
+      </ul>
+      {expenses.length > 0 && (
+        <button className="btn">
+          clear expenses
+          <MdDelete className="btn-icon" />
+        </button>
+      )}
+    </>
   );
 }
