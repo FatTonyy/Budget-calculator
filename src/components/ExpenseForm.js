384d84e01fae619ae2465f26384f62a3b43f0058
Fat-Tony <fat_tonyy@icloud.com>
2019-12-16 17:13:04 +0000

    ExpenseForm component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { MdSend } from "react-icons/md";
 
 export default function ExpenseForm() {
   return (
-    <div>
-      <h4>hello from expense form</h4>
-    </div>
+    <form>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g booze money"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
+          <input
+            type="text"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="e.g 200"
+          />
+        </div>
+      </div>
+
+      <button type="submit" className="btn">
+        submit <MdSend className="btn-icon" />
+      </button>
+    </form>
   );
 }
